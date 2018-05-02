79dae7b695edaaf83e4dcb1751e785772d82e688
Frederik Shull <zlyfer@protonmail.com>
2018-05-02 14:26:46 +0200

    update

--- ship.js
+++ ship.js
@@ -6,19 +6,31 @@ class Ship {
     this.rotation = 0;
     this.velocity = createVector(0, 0);
     this.boost = false;
+    this.hyperboost = false;
     this.acceleration = acceleration;
+    this.oacceleration = acceleration;
     this.clone = null;
     this.isClone = isClone;
     this.strength = cstrength;
+    this.sdir = this.getSDir(this.head);
   }
 
   update() {
     if (!(this.isClone)) {
       this.head += this.rotation;
       if (this.boost) {
+        if (this.hyperboost) {
+          this.acceleration = this.oacceleration * 2;
+        } else {
+          this.acceleration = this.oacceleration;
+        }
         this.move();
       }
-      this.velocity.mult(0.99);
+      if (!(this.hyperboost) && this.strength > 100) {
+        this.velocity.mult(0.96);
+      } else {
+        this.velocity.mult(0.99);
+      }
       this.position.add(this.velocity);
       this.edge();
     }
@@ -35,16 +47,111 @@ class Ship {
     if (!(this.isClone)) {
       this.strength = abs(round((abs(this.velocity.x) + abs(this.velocity.y)) / (this.acceleration * 100) * 100));
     }
+    if (this.hyperboost) {
+      this.strength = this.strength * 2;
+    }
     push();
     translate(this.position.x, this.position.y);
     rotate(this.head + PI / 2);
-    fill(random(((255 / 100) * this.strength) / 5, (255 / 100) * this.strength));
+    fill(random(((240 / 100) * this.strength) / 5, (255 / 100) * this.strength));
     strokeWeight(0);
     quad(-this.size, this.size, 0, this.size / 1.8, this.size, this.size, 0, (this.size / 1.8) + this.strength / 2.8);
     pop();
   }
 
+  getSDir() {
+    let d = round(((this.head + PI / 2) % TAU) / (TAU / 8));
+    switch (d) {
+      case 0:
+        this.sdir = "N";
+        break;
+      case 1:
+        this.sdir = "NW";
+        break;
+      case 2:
+        this.sdir = "W";
+        break;
+      case 3:
+        this.sdir = "SW";
+        break;
+      case 4:
+        this.sdir = "S";
+        break;
+      case 5:
+        this.sdir = "SE";
+        break;
+      case 6:
+        this.sdir = "E";
+        break;
+      case 7:
+        this.sdir = "NE";
+        break;
+    }
+
+
+    // fill(180);
+    // text(d, -50, 50);
+    // strokeWeight(2);
+    //
+    // push();
+    // rotate(directions.n);
+    // stroke('rgba(100,100,100,0.5)');
+    // line(0, 5, 0, 10);
+    // pop();
+    //
+    // push();
+    // rotate(directions.ne);
+    // stroke('rgba(255,0,0,0.5)');
+    // line(0, 5, 0, 10);
+    // pop();
+    //
+    // push();
+    // rotate(directions.e);
+    // stroke('rgba(255,255,0,0.5)');
+    // line(0, 5, 0, 10);
+    // pop();
+    //
+    // push();
+    // rotate(directions.se);
+    // stroke('rgba(255,255,255,0.5)');
+    // line(0, 5, 0, 10);
+    // pop();
+    //
+    // push();
+    // rotate(directions.s);
+    // stroke('rgba(0,255,0,0.5)');
+    // line(0, 5, 0, 10);
+    // pop();
+    //
+    // push();
+    // rotate(directions.sw);
+    // stroke('rgba(0,255,255,0.5)');
+    // line(0, 5, 0, 10);
+    // pop();
+    //
+    // push();
+    // rotate(directions.w);
+    // stroke('rgba(0,0,255,0.5)');
+    // line(0, 5, 0, 10);
+    // pop();
+    //
+    // push();
+    // rotate(directions.nw);
+    // stroke('rgba(255,0,255,0.5)');
+    // line(0, 5, 0, 10);
+    // pop();
+    //
+    // push();
+    // rotate((this.head + PI / 2) % TAU);
+    // strokeWeight(1)
+    // stroke('rgba(255, 255, 255,0.5)');
+    // line(-0.5, 0, -0.5, -10);
+    // pop();
+
+  }
+
   show() {
+    this.getSDir();
     push();
     translate(this.position.x, this.position.y);
     rotate(this.head + PI / 2);
@@ -52,6 +159,7 @@ class Ship {
     fill(180);
     quad(0, -this.size, -this.size, this.size, 0, this.size / 1.8, this.size, this.size);
     pop();
+
   }
 
   edge() {
@@ -90,6 +198,31 @@ class Ship {
       this.position.x = -(width / 2) + this.size;
     }
   }
+  shootedge() {
+    let m = abs(this.head) % TAU;
+    if (
+      (
+        this.position.y - this.size < -height / 2 &&
+        1
+      ) ||
+      (
+        this.position.y + this.size > height / 2 &&
+        true
+      ) ||
+      (
+        this.position.x - this.size < -width / 2 &&
+        true
+      ) ||
+      (
+        this.position.x + this.size > width / 2 &&
+        true
+      )
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 class Projectile {
@@ -110,7 +243,7 @@ class Projectile {
       this.position.y < -height / 2
     ) {
       projectiles.splice(index, 1);
-      return random([0, 1]);
+      return 1;
     }
     return 0;
   }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,35 @@
 var
   ship,
   stars = [],
-  projectiles = [],
-  shots = 0,
-  missed = 0,
-  statsE = false;
+  projectiles = [];
+var
+  hyperboost = 100;
+var
+  stats = {
+    enabled: false,
+    shots: 0,
+    missed: 0,
+    score: 0,
+    speed: 0,
+    accuracy: 0,
+    hyperboost: 100
+  };
+var directions;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ship = new Ship(0, 0, -TAU / 4, 18, 0.1);
+  directions = {
+    n: (6 / 8) * TAU,
+    ne: (7 / 8) * TAU,
+    e: (8 / 8) * TAU,
+    se: (1 / 8) * TAU,
+    s: (2 / 8) * TAU,
+    sw: (3 / 8) * TAU,
+    w: (4 / 8) * TAU,
+    nw: (5 / 8) * TAU
+  };
+  ship = new Ship(0, 0, directions.n, 18, 0.1);
   for (let s = 0; s < round((width + height) / 10); s++) {
     stars.push(new Star());
   }
@@ -20,9 +41,7 @@ function windowResized() {
 
 function draw() {
   background(0);
-  if (statsE) {
-    showStats();
-  }
+  push();
   translate(width / 2, height / 2);
   stars.forEach(star => {
     star.blink();
@@ -33,61 +52,93 @@ function draw() {
       projectile.fly();
     }
     projectile.show();
-    missed += projectile.edge(projectiles);
+    stats.missed += projectile.edge(projectiles);
   });
   ship.update();
   controls();
+  pop();
+  if (stats.enabled) {
+    showStats();
+  }
+  showHUD();
 }
 
 function controls() {
   if (keyIsDown(RIGHT_ARROW)) {
-    ship.rotation = 0.08;
+    ship.rotation = (TAU / 75);
+    ship.rotation = (TAU / 250);
   } else if (keyIsDown(LEFT_ARROW)) {
-    ship.rotation = -0.08;
+    ship.rotation = -(TAU / 75);
   } else {
     ship.rotation = 0;
   }
   if (keyIsDown(UP_ARROW)) {
     ship.boost = true;
+    if (keyIsDown(SHIFT)) {
+      if (stats.hyperboost > 0) {
+        ship.hyperboost = true;
+        stats.hyperboost--;
+      } else {
+        ship.hyperboost = false;
+      }
+    }
   } else {
     ship.boost = false;
+  }
+  if (!keyIsDown(SHIFT) || !keyIsDown(UP_ARROW)) {
+    ship.hyperboost = false;
+    if (stats.hyperboost < 100) {
+      stats.hyperboost++;
+    }
   }
 }
 
 function keyPressed() {
   switch (keyCode) {
     case 32:
-      shots++;
-      projectiles.push(new Projectile(ship));
+      stats.shots++;
+      if (ship.shootedge()) {
+        projectiles.push(new Projectile(ship));
+      }
       if (ship.clone) {
-        projectiles.push(new Projectile(ship.clone));
+        if (ship.clone.shootedge()) {
+          projectiles.push(new Projectile(ship.clone));
+        }
       }
       break;
     case 49:
-      statsE = !statsE;
+      stats.enabled = !stats.enabled;
       break;
   }
 }
 
 function showStats() {
-  let accuracy, score, speed;
-  stats = ['score', 'accuracy', 'speed'];
-
-  score = (-shots * 10);
-  speed = ship.strength;
-  if (shots > 0) {
-    accuracy = round((shots - missed) / (shots / 100)) + "%";
+  let i = 0;
+  stats.score = (-stats.shots * 10);
+  stats.speed = ship.strength;
+  if (stats.shots > 0) {
+    stats.accuracy = round((stats.shots - stats.missed) / (stats.shots / 100));
   } else {
-    accuracy = '0%';
+    stats.accuracy = 0;
   }
-
   push();
   translate(15, 30);
   strokeWeight(0);
   textSize(16);
   fill(244, 67, 54);
-  stats.forEach(key => {
-    text(`${key} ${eval(key)}`, 0, stats.indexOf(key) * 20);
-  });
+  for (key in stats) {
+    if (key != 'enabled') {
+      text(`${key}: ${stats[key]}`, 0, i * 20);
+      i++;
+    }
+  }
+  pop();
+}
+
+function showHUD() {
+  push();
+  fill(255, (230 / 100) * stats.hyperboost);
+  strokeWeight(0);
+  rect(15, height - 30, stats.hyperboost * 1.5, 15);
   pop();
 }
